c23b25e6c5318236854623c473d77b3cd11c2e03
三丙 <sanbing-os@foxmail.com>
2024-11-22 15:30:20 +0800

    docker修改为普通用户，文件统一存放到home目录

--- docker/start.sh
+++ docker/start.sh
@@ -7,15 +7,15 @@
 echo "Starting Server ..."
 
 export JAVA_APP_OPTS="-XX:+UseContainerSupport -XX:InitialRAMPercentage=10 -XX:MaxRAMPercentage=70 \
-                                 -Xlog:gc*,heap*,age*,safepoint=debug:file=/var/log/sanbing/gc/gc.log:time,uptime,level,tags:filecount=10,filesize=10M \
+                                 -Xlog:gc*,heap*,age*,safepoint=debug:file=/home/sanbing/logs/gc/gc.log:time,uptime,level,tags:filecount=10,filesize=10M \
                                  -XX:+HeapDumpOnOutOfMemoryError \
-                                 -XX:HeapDumpPath=/var/log/sanbing/heapdump/ \
+                                 -XX:HeapDumpPath=/home/sanbing/logs/heapdump/ \
                                  -XX:+UseTLAB -XX:+ResizeTLAB -XX:+PerfDisableSharedMem -XX:+UseCondCardMark \
                                  -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:+UseStringDeduplication -XX:+ParallelRefProcEnabled -XX:MaxTenuringThreshold=10 \
                                  -Xss512k -XX:G1ReservePercent=20 \
                                  -XX:-OmitStackTraceInFastThrow \
-                                 -Dlogging.config=/app/config/log4j2.xml"
+                                 -Dlogging.config=/home/sanbing/config/log4j2.xml"
 
 #export JAVA_OPTS_EXTEND="-Xdebug -Xrunjdwp:transport=dt_socket,address=0.0.0.0:8000,server=y,suspend=n"
 
-exec java $JAVA_APP_OPTS $JAVA_OPTS_EXTEND $JAVA_OPTS -Dnetworkaddress.cache.ttl=60 -jar /app/application.jar
+exec java $JAVA_APP_OPTS $JAVA_OPTS_EXTEND $JAVA_OPTS -Dnetworkaddress.cache.ttl=60 -jar application.jar
