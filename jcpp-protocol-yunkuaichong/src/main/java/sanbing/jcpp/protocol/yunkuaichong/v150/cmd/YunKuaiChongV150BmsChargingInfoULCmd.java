10dd03bbb3525cd82c40cfcda5fcc6c88e644534
三丙 <10604541+sanbing-os@user.noreply.gitee.com>
2025-08-01 15:21:41 +0800

    补充license

--- jcpp-protocol-yunkuaichong/src/main/java/sanbing/jcpp/protocol/yunkuaichong/v150/cmd/YunKuaiChongV150BmsChargingInfoULCmd.java
+++ jcpp-protocol-yunkuaichong/src/main/java/sanbing/jcpp/protocol/yunkuaichong/v150/cmd/YunKuaiChongV150BmsChargingInfoULCmd.java
@@ -1,3 +1,9 @@
+/**
+ * 开源代码，仅供学习和交流研究使用，商用请联系三丙
+ * 微信：mohan_88888
+ * 抖音：程序员三丙
+ * 付费课程知识星球：https://t.zsxq.com/aKtXo
+ */
 package sanbing.jcpp.protocol.yunkuaichong.v150.cmd;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
